847451e40c5681e8422e645945316aff7f843337
DmitriiBogomolov <foreo1997@gmail.com>
2023-05-31 07:01:41 +1000

    make tests basic structure

--- auth/tests/functional/conftest.py
+++ auth/tests/functional/conftest.py
@@ -6,6 +6,7 @@ from psycopg2.extensions import connection, cursor
 from flask import Flask
 from flask.testing import FlaskClient, FlaskCliRunner
 from redis import Redis
+from flask_jwt_extended import create_access_token
 
 from app import create_app
 from config import config
@@ -58,6 +59,16 @@ def app(db: PSQL, redis: Redis) -> Flask:
 @pytest.fixture()
 def client(app: Flask) -> FlaskClient:
     return app.test_client()
+
+
+@pytest.fixture(scope='function')
+def jwt_headers(app: Flask) -> FlaskClient:
+    with app.app_context():
+        access_token = create_access_token({'id': '11111169-6712-4666-8116-7c4eaf111111'})
+        headers = {
+            'Authorization': 'Bearer {}'.format(access_token)
+        }
+        return headers
 
 
 @pytest.fixture()
