0d76cc1eed7964ec0bcb578b34e782a6f547a550
DmitriiBogomolov <foreo1997@gmail.com>
2023-07-06 03:02:57 +1000

    add ugc_api mvp

---
+++ ugc/src/core/config.py
@@ -0,0 +1,46 @@
+from logging import config as logging_config
+
+from pydantic import BaseSettings
+
+from src.core.logger import LOGGING
+
+logging_config.dictConfig(LOGGING)
+
+
+class Base(BaseSettings):
+    class Config:
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+
+
+class AppConfig(Base):
+    project_name: str = 'ugc_service'
+    redis_host: str = 'redis'
+    redis_port: int = 6379
+    redis_db: int = 2
+
+    authjwt_secret_key: str
+
+
+class KafkaConfig(Base):
+    host: str = 'localhost'
+    port: int = 9092
+    topic_name: list = ['views']
+    auto_offset_reset: str = 'earliest'
+    group_id: str = 'clickhouse'
+    batch: int = 10000
+
+    class Config:
+        env_prefix = 'kafka_'
+
+    @property
+    def params(self):
+        return {
+            'bootstrap_servers': '{}:{}'.format(self.host, self.port),
+            'auto_offset_reset': self.auto_offset_reset,
+            'group_id': self.group_id
+        }
+
+
+config = AppConfig()
+kafka_config = KafkaConfig()
---
+++ ugc/src/models.py
@@ -0,0 +1,16 @@
+from uuid import UUID
+
+from pydantic import BaseModel, Field, validator
+
+
+class ViewEventModel(BaseModel):
+    movie_id: UUID
+    lenght_movie: int = Field(ge=60, le=(24 * 60 * 60))  # total movie lenth
+    duration: int = Field(ge=0)  # last viewed point of the movie (in seconds)
+
+    @validator('duration')
+    def must_be_lte_movie_lenght(cls, v, values, **kwargs):
+        if v > values['lenght_movie']:
+            raise ValueError('The current duration must be '
+                             'lower or equal total movie lenght.')
+        return v
