d02c035dde42ca7fb3f0847e4a3d989bd99de814
Igor Vasin <i.vasin@erlyvideo.ru>
2023-07-25 11:50:00 +0300

    corrected paragraphs 1 and 2 of the reviewer's comments

--- auth/app/services/sdk_servise.py
+++ auth/app/services/sdk_servise.py
@@ -1,10 +1,11 @@
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
+from app.core.config import config
 
 
 def init_sdk_service():
     sentry_sdk.init(
-        dsn='https://e91f901f7316439ea928ff90dec00904@o4505504683655168.ingest.sentry.io/4505562211221504',
+        dsn=config.dsn,
         integrations=[
             FlaskIntegration(),
         ],
